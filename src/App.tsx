de33ec1c1232b251406abb58cd7cd54d0cabf7e6
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-09-12 10:50:19 +0600

    Created useState for people and changed output

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
 import Person from "./components/Person/Person.tsx";
+import {useState} from "react";
 
+interface IPerson {
+    name: string;
+    age: number;
+    hobby?:  string | null,
+}
 const App = () => {
-
+    const [people, setPeople] = useState<IPerson[]>([
+        {name: "John", age: 28, hobby: "Coding"},
+        {name: "Jane", age: 30},
+    ]);
 
   return (
     <>
-       <Person name="John" age="30"/>
+       <Person name={people[0].name} age={people[0].age}>
+           <p><b>Hobby:</b> {people[0].hobby}</p>
+       </Person>
+        <Person name={people[1].name} age={people[1].age}>
+        </Person>
     </>
   )
 };
